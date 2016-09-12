09b2e4dd6a2d519d26e3bf4ad144e6d85416ef84
AM Infotrix <AM_Infotrix@Monalisa>
2016-09-12 18:20:46 +0530

    .

--- php/include/Config.php
+++ php/include/Config.php
@@ -5,20 +5,20 @@
  * Database configuration
  
  */
-define ( 'DB_USERNAME', 'root' );
-define ( 'DB_PASSWORD', '' );
-define ( 'DB_HOST', 'localhost' );
-define ( 'DB_NAME', 'beat' );
-define ( 'DB_PORT', 3306 );
+// define ( 'DB_USERNAME', 'root' );
+// define ( 'DB_PASSWORD', '' );
+// define ( 'DB_HOST', 'localhost' );
+// define ( 'DB_NAME', 'beat' );
+// define ( 'DB_PORT', 3306 );
 
 /**
  * for server
  */
-// define ( 'DB_USERNAME', 'the4thofficial_ami1250000' );
-// define ( 'DB_PASSWORD', 'pass@ami1250000' );
-// define ( 'DB_HOST', 'localhost' );
-// define ( 'DB_NAME', 'the4thofficial_matches' );
-// define ( 'DB_PORT', 3306 );
+define ( 'DB_USERNAME', 'assaisol_beat008' );
+define ( 'DB_PASSWORD', 'aminfotrix008' );
+define ( 'DB_HOST', 'localhost' );
+define ( 'DB_NAME', 'assaisol_AMi_beat' );
+define ( 'DB_PORT', 3306 );
 
 define ( 'QUERY_EXCEPTION', 'Please check mysql query. Fatal error' );
 
@@ -34,18 +34,18 @@ define ( "DATA_FOUND", "Data found" );
 
 define ( "DATA_NOT_FOUND", "No data found" );
 
-define("FB_USER_ID","Facebook user id");
+define ( "FB_USER_ID", "Facebook user id" );
 
-define("NAME", "Name");
+define ( "NAME", "Name" );
 
-define("ACCESS_TOKEN", "Access token");
+define ( "ACCESS_TOKEN", "Access token" );
 
-define("USER_ID", "User id");
+define ( "USER_ID", "User id" );
 
-define("LEFT_HIT_COUNT", "Left hit count");
+define ( "LEFT_HIT_COUNT", "Left hit count" );
 
-define("RIGHT_HIT_COUNT", "Right hit count");
+define ( "RIGHT_HIT_COUNT", "Right hit count" );
 
-define("USER_IMAGE","user image");
+define ( "USER_IMAGE", "user image" );
 
 ?>
